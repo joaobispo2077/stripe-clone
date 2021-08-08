9087732f99fe1773ff88d452f239faf374460467
João Bispo <joaobispo2077@gmail.com>
2021-08-08 10:52:43 -0300

    refactor: fallback to react-app project

--- src/styles/themes/renderTheme.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from '.';
-
-export const renderTheme = (children: React.ReactNode) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
--- src/templates/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as Styled from './styles';
-
-export function Home() {
-  return (
-    <Styled.Container>
-      <h1>Hello World</h1>
-    </Styled.Container>
-  );
-}
